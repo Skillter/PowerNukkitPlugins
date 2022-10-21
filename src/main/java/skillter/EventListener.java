a85cb0313baee895cfcccf9a5647f6aa3792887c
Skillter <57605954+Skillter@users.noreply.github.com>
2022-10-21 14:02:09 +0200

    Initial upload

---
+++ src/main/java/skillter/EventListener.java
@@ -0,0 +1,15 @@
+package skillter;
+
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerDeathEvent;
+
+public class EventListener implements Listener {
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e) {
+        if (Main.isEnabled && Main.isActive) {
+            e.setKeepExperience(true);
+        }
+    }
+}
